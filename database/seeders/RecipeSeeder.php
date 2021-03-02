890fafc402acebb6f3270b2d52462135a79f4438
Justin <justinpoot@live.nl>
2021-03-02 23:20:43 +0100

    Add recipe model and controller. Add recipe migration. Add recipe seeder.

---
+++ database/seeders/RecipeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RecipeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('recipes')->insert([
+            'title' => 'Risotto',
+            'description' => 'Chicken Risotto'
+        ]);
+    }
+}
