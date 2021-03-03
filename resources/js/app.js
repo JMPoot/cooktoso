3ca6a3686fc911f4f79fe9bdfea378cb3a7a052c
Justin <justinpoot@live.nl>
2021-03-03 21:35:52 +0100

    Add vue to project.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+
+Vue.component('foo', require('./components/Foo.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/foo', function () {
-    return 'foo';
+Route::get('/home', function() {
+    return view('home');
 });
